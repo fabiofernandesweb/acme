238fb6873f7833eb594445a749f48f9b97014401
fabiofernandesweb <fabiofgomes.fg@gmail.com>
2026-03-05 22:32:37 +0000

    fix: corrigir nomenclatura de propriedade

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <main>
-      <h1>Sistema em construção.</h1>
+      <h1 className="bg-red-700 text-green-400 text-3xl">Sistema em construção.</h1>
     </main>
   );
 };
